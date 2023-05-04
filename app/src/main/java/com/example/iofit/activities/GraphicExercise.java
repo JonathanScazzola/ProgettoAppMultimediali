0e4686435473dbaa532df7b49bfcdaa637f7aa93
lorenzo-mannini <lollo.mannini@gmail.com>
2023-05-04 21:33:34 +0200

    Aggiunte schermate

---
+++ app/src/main/java/com/example/iofit/activities/GraphicExercise.java
@@ -0,0 +1,16 @@
+package com.example.iofit.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.iofit.R;
+
+public class GraphicExercise extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_graphic_exercise);
+    }
+}
